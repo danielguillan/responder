2a55203ac16c815b49a3cd1338c23e83a70a27c6
Daniel Guillan <daniel.guillan@gmail.com>
2012-09-26 19:30:52 +0200

    Initial mixin functionality. Responds to breakpoints, groups and something I miserably call extends (and-up, and-below, only)

--- lib/respondto.rb
+++ lib/respondto.rb
@@ -3,6 +3,17 @@ root = File.join(File.dirname(__FILE__), "..")
 require "respondto/version"
 require "compass"
 
+module Sass::Script::Functions
+  def regex(string,regex)
+    assert_type string, :String
+    assert_type regex, :String
+    /#{regex.value}/.match string.value
+    $1 ? match = $1 : match = ''
+    Sass::Script::String.new(match)
+  end
+  declare :regex, :args => [:string, :string]
+end
+
 Compass::Frameworks.register("respondto",
   :stylesheets_directory => File.join(root,"scss")
 )
---
+++ test/config.rb
@@ -0,0 +1,10 @@
+require File.join(File.dirname(__FILE__),"../lib/respondto")
+
+project_type = :stand_alone
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "img"
+
+preferred_syntax = :scss
+relative_assets = true
